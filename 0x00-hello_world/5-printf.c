1c3c2bee4aa0ec7b68b2dde01de437bf0f33c3d0
Tfstar <pacesetter05@gmail.com>
2022-06-02 18:21:39 -0700

    hi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,10 @@
  *
  * main - Entry point
  *
- * Return : Always 0 (success)
+ * Return: Always 0 (success)
  * */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 return (0);
 }
