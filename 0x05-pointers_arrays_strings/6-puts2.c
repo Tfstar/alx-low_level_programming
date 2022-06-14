870021603c832425ef2e0f23b628762c86daf9dd
Tfstar <pacesetter05@gmail.com>
2022-06-13 20:15:07 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *puts2 - prints every other character of a string,
+ *@str: A pointer to an int that will be updated
+ *
+ *Return: void
+ */
+
+void puts2(char *str)
+{
+	int x; /*Declaring variables*/
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (x % 2 == 0)
+		_putchar (str[x]);
+	}
+
+	_putchar ('\n');
+}
